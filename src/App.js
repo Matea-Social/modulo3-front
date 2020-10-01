c5b3a290558e5d5bc2015ff779b65247e384b80e
Martin Varela <martinvarela92@gmail.com>
2020-10-01 16:10:25 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [usuarios, setUsuarios] = useState([]);
+  const [songs, setSongs] = useState([]);
+
+  const obtenerUsuarios = async () => {
+    // obtiene los usuarios desde el backend
+    const response = await fetch("http://localhost:4000/users/");
+    const data = await response.json();
+    setUsuarios(data);
+  };
+
+  useEffect(() => {
+    // Esto se ejecuta al renderizar la pagina por primera vez
+    obtenerUsuarios();
+  }, []);
+
+  const handleClick = async (e, usuario) => {
+    e.preventDefault();
+    console.log("Se hizo click en el usaurio ", usuario);
+    // Se le pega a la API que trae las canciones
+    const response = await fetch(`http://localhost:4000/songs/`);
+    const data = await response.json();
+    setSongs(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>Lista de usuarios</p>
+        {usuarios.length > 0 ? (
+          usuarios.map((usuario) => (
+            <p className="Usuario" key={usuario._id}>
+              {usuario.name}
+              <button onClick={(e) => handleClick(e, usuario)}>
+                Canciones
+              </button>
+            </p>
+          ))
+        ) : (
+          <p>Cargando...</p>
+        )}
+      </div>
+      <div>
+        <p>Lista de canciones</p>
+        {songs.map((song) => (
+          <p className="Song" key={song._id}>
+            {song.name}
+          </p>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
